9c27e1cd54a7682c5a15a06f2307509183d14193
Kean <50844146+Mojiez@users.noreply.github.com>
2022-01-27 16:01:56 +0100

    Added some css Snass

---
+++ ComponentShowCase/ComponentShowCase/DynamicColorChanges/ColorController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ComponentShowCase.DynamicColorChanges
+{
+    public class ColorController
+    {
+        public string Linear_Gradient { get; set; }
+        public string Radial_Gradient { get; set; }
+        public EventHandler ColorChanged { get; set; }
+        private CssGenerator _generator;
+
+        public ColorController()
+        {
+            _generator = new();
+        }
+
+        public string GetLinearGradient(string rgbValues1, string rgbValues2)
+        {
+            _generator.Reset();
+            _generator.AddLinearGradient(rgbValues1, rgbValues2);
+            return Linear_Gradient = _generator.GetCss;
+        }
+
+        public string GetRadialGradient(string rgbValues1, string rgbValues2)
+        {
+            _generator.Reset();
+            _generator.AddRadialGradient(rgbValues1, rgbValues2);
+            _generator.AddBorder(2, "solid", "black");
+            return Radial_Gradient = _generator.GetCss;
+        }
+
+        public async void Start()
+        {
+            Random random = new();
+            while (true)
+            {
+                string red = random.Next(256).ToString();
+                string green = random.Next(256).ToString();
+                string blue = random.Next(256).ToString();
+                GetLinearGradient($"{red},{green},{blue}", $"{blue},{green},{red}");
+                GetRadialGradient($"{red},{green},{blue}", $"{blue},{green},{red}");
+                ColorChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Linear_Gradient)));
+                ColorChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Radial_Gradient)));
+                await Task.Delay(100);
+            }
+        }
+    }
+}
---
+++ ComponentShowCase/ComponentShowCase/DynamicColorChanges/CssGenerator.cs
@@ -0,0 +1,34 @@
+﻿namespace ComponentShowCase.DynamicColorChanges
+{
+    public class CssGenerator
+    {
+        private string _css;
+
+        public enum Borders { solid, dotted }
+
+        public string GetCss { get => _css; }
+        public void AddLinearGradient(string rgbValues1, string rgbValues2)
+        {
+            _css += $"background: linear-gradient(rgb({rgbValues1}) 20%, rgb({rgbValues2}) 80%);";
+        }
+        public void AddRadialGradient(string rgbValues1, string rgbValues2)
+        {
+            _css += $"background: radial-gradient(rgb({rgbValues1}) 20%, rgb({rgbValues2}) 80%);";
+        }
+
+        public void AddBorder(int thickness, string borderType, string color) 
+        {
+            _css += $"border:{thickness}px {borderType} {color};";
+        }
+
+        public void AddTextColor(string color) 
+        {
+            _css += $"color:{color};";
+        }
+
+        public void Reset() 
+        {
+            _css = "";
+        }
+    }
+}
